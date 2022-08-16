227bd3db122a88238950f8296bc4ecab0cbaa1d3
Aleg3000 <88538863+Aleg3000@users.noreply.github.com>
2022-08-16 18:27:32 +0530

    add bridge creator

--- app/client/app/game/world/worldMap/WorldMap.ts
+++ app/client/app/game/world/worldMap/WorldMap.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Mesh, MeshBasicMaterial, NearestFilter, Object3D, PlaneBufferGeometry, Texture } from 'three';
 import { EProportions } from '../../../../../ts/enums';
 import { IStartGameOptions } from '../../../../../ts/interfaces';
 import Perlin from '../../../../utils/p5/Perlin';
 import Random from '../../../../utils/Random';
 import AssetsManager from '../../assetsManager/AssetsManager';
+import { ChosenPlace, Place, x, xy } from './interfaces';
 
 export default class WorldMap {
     private object3D: Object3D | null = null;
@@ -91,11 +93,20 @@ export default class WorldMap {
             this.drawBaseMaskMatrix(ctx, width, height);
 
             const maskData = ctx.getImageData(0, 0, width, height).data;
-            const maskMatrix = this.imageDataToMatrix(maskData, width, height);
+            let maskMatrix = this.imageDataToMatrix(maskData, width, height);
+
+            // balki start
+
+            const potentialPlaces = this.findPlacesForBridge(maskMatrix);
+            const place = this.findPlace(potentialPlaces);
+            maskMatrix = this.addBridgeToMaskMatrix(maskMatrix, potentialPlaces, place);
+
+            //balki end
 
             this.clearCanvas();
 
             this.drawGroundPattern(ctx, width, height);
+
             this.clipGroundPatternByMatrix(ctx, maskMatrix);
 
             const groundImageUrl = element.toDataURL();
@@ -308,4 +319,101 @@ export default class WorldMap {
 
         return this.getObjectPlace(matrix, width, height, attempt - 1);
     };
+
+    private addBridgeToMaskMatrix(
+        matrix: number[][],
+        potentialPlaces: (Place | undefined)[],
+        place: { y: { start: number | undefined; end: number; height: number } } | null | undefined
+    ): number[][] {
+        if (potentialPlaces.length === 0 || !potentialPlaces[0] || !place) return matrix;
+        const yStart = place?.y?.start || 0 + 50; /* bridge step */
+        const i = potentialPlaces.findIndex((el: any) => el.y === yStart);
+        if (i === -1) return matrix;
+        const placeForBridge = potentialPlaces[i];
+        const xStart = placeForBridge?.x.start || 0;
+        const xWidth = placeForBridge?.x.width || 0;
+        // console.log(xStart, yStart, xWidth);
+        const bWidth = 50; /* bridge step */
+        const bHeight = 10; /* bridge height*/
+        for (let i = 0; i <= bWidth; i++) {
+            for (let a = 0; a <= bHeight; a++) {
+                matrix[yStart - i - a][xStart + i] = 1;
+                matrix[yStart - i - a][xStart + xWidth - i] = 1;
+            }
+        }
+        const width = xWidth - bWidth * 2;
+        for (let i = 0; i <= bHeight; i++) {
+            matrix[yStart - bWidth - i].splice(xStart + bWidth, width, ...new Array(width).fill(1));
+        }
+
+        return matrix;
+    }
+
+    // bridge create
+
+    private findPlacesForBridge(matrix: number[][]) {
+        return matrix
+            .map((el: number[], index: number) => {
+                const arrX: x[] = [];
+                let start = 1;
+                let end = 1;
+                for (let i = 0; i <= el.length; i++) {
+                    if (el[i] === el[i + 1]) {
+                        continue;
+                    } else {
+                        end = i;
+                        const width: number = end - start;
+                        arrX.push({ type: el[i], xStart: start, xEnd: end, width });
+                        start = i + 1;
+                    }
+                }
+                return { y: index, x: arrX };
+            })
+            .filter((el: xy): boolean => el.x.length >= 5)
+            .map((el: xy) => {
+                el.x = el.x.slice(1, -1);
+                return el;
+            })
+            .map((el: xy) => {
+                const index: number = el.x.findIndex(
+                    (j: any) => j.width > 500 /* min width for bridge*/ && j.type === 0
+                );
+                if (index === -1) return null;
+                el.x = el.x.slice(index, index + 1);
+                return el;
+            })
+            .filter((el: xy | null) => !!el)
+            .map((el: xy | null /* idk why but ts want*/) => {
+                if (el)
+                    return {
+                        y: el.y,
+                        x: {
+                            start: el.x[0].xStart,
+                            end: el.x[0].xEnd,
+                            width: el.x[0].width,
+                        },
+                    };
+            });
+    }
+
+    private findPlace(allPlaces: (Place | undefined)[]) {
+        if (allPlaces.length === 0) return;
+        let places = [];
+        let start = allPlaces[0]?.y;
+        let height = 1;
+        for (let i = 0; i < allPlaces.length - 1; i++) {
+            const currentPoint = allPlaces[i]?.y || 0;
+            const nextPoint = allPlaces[i + 1]?.y;
+            if (currentPoint + 1 !== nextPoint || i === allPlaces.length - 2) {
+                const end = currentPoint;
+                places.push({ y: { start, end, height } });
+                start = allPlaces[i + 1]?.y;
+                height = 1;
+            } else height++;
+        }
+        // ищем долину с высотой хотя бы 300 и возвращаем самую верхнюю, потом можно дописать и создавать несколько балок
+        places = places.filter((el) => el.y.height > 50);
+        if (places[0].y.start) return places[0];
+        else return null;
+    }
 }
---
+++ app/client/app/game/world/worldMap/interfaces.ts
@@ -0,0 +1,6 @@
+export type x = { type: number; xStart: number; xEnd: number; width: number };
+export type xy = { y: number; x: x[] };
+
+export type Line = { start: number; end: number; width?: number; height?: number };
+export type Place = { y: number; x: Line };
+export type ChosenPlace = { y: Line };
