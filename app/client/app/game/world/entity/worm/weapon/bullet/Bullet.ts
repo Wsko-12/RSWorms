973db04f279acf05c17af31c4f4429aa6fe909e0
Wsko-12 <vaskovladislav@gmail.com>
2022-08-19 14:20:45 +0300

    feat: add remove method to Entity

--- app/client/app/game/world/entity/worm/weapon/bullet/Bullet.ts
+++ app/client/app/game/world/entity/worm/weapon/bullet/Bullet.ts
@@ -1,15 +1,23 @@
 import { CircleBufferGeometry, Mesh, MeshBasicMaterial, Object3D } from 'three/src/Three';
+import { TRemoveEntityCallback } from '../../../../../../../../ts/types';
 import { Point2 } from '../../../../../../../utils/geometry';
+import MapMatrix from '../../../../worldMap/mapMatrix/MapMatrix';
 import Entity from '../../../Entity';
 
 export default class Bullet extends Entity {
     protected object3D: Object3D;
-    constructor(id: string, position: Point2) {
-        super(id, 10, position.x, position.y);
-
+    protected explosionRadius = 100;
+    constructor(removeEntityCallback: TRemoveEntityCallback, id: string, position: Point2) {
+        super(removeEntityCallback, id, 10, position.x, position.y);
+        this.physics.friction = 0.5;
         const geometry = new CircleBufferGeometry(this.radius, 120);
         const material = new MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.5 });
         this.object3D = new Mesh(geometry, material);
         this.object3D.position.set(position.x, position.y, 0);
+    }
+
+    protected handleCollision(mapMatrix: MapMatrix): void {
+        super.handleCollision(mapMatrix);
+        this.remove();
     }
 }
