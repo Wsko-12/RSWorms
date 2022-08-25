dd70a6e554c6e4fa339dbbc7c48f1713675a17df
Aleg3000 <88538863+Aleg3000@users.noreply.github.com>
2022-08-25 19:50:14 +0530

    feat: basic lobby

--- app/client/app/lobby/Lobby.ts
+++ app/client/app/lobby/Lobby.ts
@@ -1,242 +1,130 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { EMapPacksDecorItems, EMapPacksNames, EWorldSizes } from '../../../ts/enums';
 import { TStartGameCallback } from '../../../ts/types';
 import PageBuilder from '../../utils/pageBuilder';
-import { IStartGameOptions } from '../../../ts/interfaces';
+import { IGMLoops, IStartGameOptions } from '../../../ts/interfaces';
 import './style.scss';
+import { Context } from 'vm';
+import Loop from '../game/loop/Loop';
 
 export default class Lobby {
+    // private loops: IGMLoops;
+    clouds: any[] = [];
+    cloud: any;
+    mainScreen: any;
+    customGameScreen: any;
+    lobbyWrapper: any;
+    canvasHeight = 0;
+    canvasWidth = 0;
+    windowHeight = 0;
+    windowWidth = 0;
+    ctx: Context | null = null;
     private startGameCallback: TStartGameCallback;
     constructor(startGameCallback: TStartGameCallback) {
         this.startGameCallback = startGameCallback;
-    }
-    public start() {
-        this.buildLobby();
-        // button.onclick = () => {
-        // const seed = Math.random();
-        // const seed = 0.7135371756374531;
-        // const seed = 0.7972989657842342;
-        // const seed = 0.7190317696597344;
-        // const seed = 0.4884739715122959;
 
-        //worms bug
-        //const seed = 0.6469262503466888;
+        this.createLobby();
+        this.createMainScreen();
+        this.createCustomGameScreen();
+        this.mainScreen.scrollIntoView();
+        this.createClouds();
+        this.loop();
 
-        // };
-    }
-
-    private buildLobby() {
-        const title: HTMLElement = PageBuilder.createElement('div', {
-            classes: 'title',
-            content: 'WORMS',
+        document.addEventListener('keydown', () => {
+            this.customGameScreen.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
         });
-
-        const header: HTMLElement = PageBuilder.createElement('header', {
-            classes: 'header',
-            content: title,
-        });
-
-        const freeGameBtn: HTMLButtonElement = PageBuilder.createElement<HTMLButtonElement>('button', {
-            classes: ['free-game-btn', 'lobby-btn'],
-            content: 'Start free game',
-        });
-
-        const seed = 0.711119400099296;
-        console.log('Seed: ', seed);
-        const defaultOptions = {
-            mapTexturePackName: EMapPacksNames.candy,
-            worldSize: EWorldSizes.medium,
-            seed,
-            decor: {
-                count: EMapPacksDecorItems[EMapPacksNames.candy],
-                max: 6,
-                min: 2,
-            },
-        };
-
-        freeGameBtn.addEventListener('click', () => {
-            this.startGame(defaultOptions);
-        });
-
-        const customGameBtn: HTMLButtonElement = PageBuilder.createElement<HTMLButtonElement>('button', {
-            classes: ['custom-game-btn', 'lobby-btn'],
-            content: 'Start custom game',
-        });
-
-        const customTitle: HTMLElement = PageBuilder.createElement('div', {
-            classes: 'custom-title',
-            content: 'Game Settings',
-        });
-
-        const optionMoon: HTMLOptionElement = PageBuilder.createElement<HTMLOptionElement>('option', {
-            attrs: {
-                value: 'moon',
-                selected: true,
-            },
-            content: 'Moon',
-        });
-
-        const optionCandy: HTMLOptionElement = PageBuilder.createElement<HTMLOptionElement>('option', {
-            attrs: {
-                value: 'candy',
-            },
-            content: 'Candy',
-        });
-
-        const mapTextureSel: HTMLSelectElement = PageBuilder.createElement<HTMLSelectElement>('select', {
-            classes: 'map-texture-sel',
-            attrs: {
-                name: 'mapTextureSel',
-            },
-            content: [optionMoon, optionCandy],
-        });
-
-        mapTextureSel.addEventListener('change', (e) => {
-            const element = e.target as HTMLSelectElement;
-            (
-                document.querySelector('.texture-img') as HTMLImageElement
-            ).src = `../../assets/mapPacks/${element.value}/ground.png`;
-        });
-
-        const mapTextureSelWrapper: HTMLElement = PageBuilder.createElement('div', {
-            classes: 'map-texture-sel-wrapper',
-            content: [
-                mapTextureSel,
-                `<img class='texture-img' width='50' height='50' src='../../assets/mapPacks/moon/ground.png'>`,
-            ],
-        });
-
-        const mapSmall: HTMLInputElement = PageBuilder.createElement<HTMLInputElement>('input', {
-            attrs: {
-                type: 'radio',
-                name: 'map-size',
-                id: 'small',
-                value: 'small',
-            },
-        });
-
-        const mapMedium: HTMLInputElement = PageBuilder.createElement<HTMLInputElement>('input', {
-            attrs: {
-                type: 'radio',
-                name: 'map-size',
-                id: 'medium',
-                value: 'medium',
-                checked: true,
-            },
-        });
-
-        const mapBig: HTMLInputElement = PageBuilder.createElement<HTMLInputElement>('input', {
-            attrs: {
-                type: 'radio',
-                name: 'map-size',
-                id: 'big',
-                value: 'big',
-            },
-        });
-
-        const mapSizeSelWrapper: HTMLElement = PageBuilder.createElement('div', {
-            classes: 'map-size-sel-wrapper',
-            content: [
-                mapSmall,
-                `<label for="small">Small</label>`,
-                mapMedium,
-                `<label for="medium">Medium</label>`,
-                mapBig,
-                `<label for="big">Big</label>`,
-            ],
-        });
-
-        const startCustomGameBtn: HTMLButtonElement = PageBuilder.createElement<HTMLButtonElement>('button', {
-            classes: 'button',
-            content: 'Start game',
-        });
-
-        const canselCustomGameBtn: HTMLButtonElement = PageBuilder.createElement<HTMLButtonElement>('button', {
-            classes: 'button',
-            content: 'Cansel',
-        });
-
-        const customGameSet: HTMLElement = PageBuilder.createElement('div', {
-            classes: 'custom-game-set',
-            content: [
-                customTitle,
-                '<div>Map texture:</div>',
-                mapTextureSelWrapper,
-                `<div>Map size:</div>`,
-                mapSizeSelWrapper,
-                startCustomGameBtn,
-                canselCustomGameBtn,
-            ],
-        });
-
-        customGameBtn.addEventListener('click', () => {
-            customGameSet.classList.add('open');
-        });
-
-        startCustomGameBtn.addEventListener('click', () => {
-            this.startCustomGame();
-        });
-
-        canselCustomGameBtn.addEventListener('click', () => {
-            customGameSet.classList.remove('open');
-        });
-
-        const settingsBtn: HTMLButtonElement = PageBuilder.createElement<HTMLButtonElement>('button', {
-            classes: ['settings-game-btn', 'lobby-btn'],
-            content: 'Settings',
-        });
-
-        const nonameGameBtn: HTMLButtonElement = PageBuilder.createElement<HTMLButtonElement>('button', {
-            classes: ['noname-game-btn', 'lobby-btn'],
-            content: '?',
-        });
-
-        const btnsWrapper: HTMLElement = PageBuilder.createElement('div', {
-            classes: 'btns-wrapper',
-            content: [freeGameBtn, customGameBtn, settingsBtn, nonameGameBtn],
-        });
-
-        const main: HTMLElement = PageBuilder.createElement('main', {
-            classes: 'main',
-            content: btnsWrapper,
-        });
-
-        document.body.append(customGameSet, header, main);
     }
 
-    private startCustomGame() {
-        const seed = 0.711119400099296;
-        const options: IStartGameOptions = {
-            mapTexturePackName: EMapPacksNames.candy,
-            worldSize: EWorldSizes.medium,
-            seed,
-            decor: {
-                count: 13,
-                max: 6,
-                min: 2,
-            },
-        };
+    createLobby() {
+        this.lobbyWrapper = PageBuilder.createElement('div', { id: 'lobby-wrapper' });
+        document.body.append(this.lobbyWrapper);
+        this.windowHeight = document.documentElement.clientHeight;
+        this.windowWidth = document.documentElement.clientWidth;
+        // this.windowHeight = window.innerHeight;
+        // this.windowWidth = window.innerWidth;
+        this.canvasWidth = this.windowWidth * 3;
+        this.canvasHeight = this.windowHeight * 3;
+        this.lobbyWrapper.style.width = `${this.windowWidth * 3}px`;
+        this.lobbyWrapper.style.height = `${this.windowHeight * 3}px`;
+        this.lobbyWrapper.innerHTML += `<canvas id='canvas-lobby' width='${this.canvasWidth}px' height='${this.canvasHeight}px'></canvas>`;
+        const canvas = document.getElementById('canvas-lobby') as HTMLCanvasElement;
+        this.ctx = canvas.getContext('2d');
+    }
 
-        const texture = (document.querySelector('.map-texture-sel') as HTMLSelectElement).value;
-        options.mapTexturePackName = texture === 'moon' ? EMapPacksNames.moon : EMapPacksNames.candy;
-        const mapSize: string = (document.querySelector(`input[name='map-size']:checked`) as HTMLInputElement).value;
+    createMainScreen() {
+        this.mainScreen = PageBuilder.createElement('div', { classes: 'main-screen' });
+        this.mainScreen.style.width = this.windowWidth + 'px';
+        this.mainScreen.style.height = this.windowHeight + 'px';
+        this.mainScreen.style.top = this.windowHeight + 'px';
+        this.mainScreen.style.left = this.windowWidth + 'px';
 
-        switch (mapSize) {
-            case 'small':
-                options.worldSize = EWorldSizes.small;
-                break;
-            case 'medium':
-                options.worldSize = EWorldSizes.medium;
-                break;
-            case 'big':
-                options.worldSize = EWorldSizes.big;
-                break;
-            default:
-                options.worldSize = EWorldSizes.medium;
+        const quickGameBtn = PageBuilder.createElement('div', { classes: 'main-screen-button' });
+        const customGameBtn = PageBuilder.createElement('div', { classes: 'main-screen-button' });
+        const networkGameBtn = PageBuilder.createElement('div', { classes: 'main-screen-button' });
+        const settingBtn = PageBuilder.createElement('div', { classes: 'main-screen-button' });
+        this.mainScreen.append(quickGameBtn, customGameBtn, networkGameBtn, settingBtn);
+
+        this.lobbyWrapper.append(this.mainScreen);
+    }
+
+    createCustomGameScreen() {
+        this.customGameScreen = PageBuilder.createElement('div', { classes: 'custom-game-screen' });
+        this.customGameScreen.style.width = this.windowWidth + 'px';
+        this.customGameScreen.style.height = this.windowHeight + 'px';
+        this.customGameScreen.style.top = '0px';
+        this.customGameScreen.style.left = this.windowWidth * 2 + 'px';
+
+        const quickGameBtn = PageBuilder.createElement('div', { classes: 'custom-game-screen-button' });
+        const customGameBtn = PageBuilder.createElement('div', { classes: 'custom-game-screen-button' });
+        const networkGameBtn = PageBuilder.createElement('div', { classes: 'custom-game-screen-button' });
+        const settingBtn = PageBuilder.createElement('div', { classes: 'custom-game-screen-button' });
+        this.customGameScreen.append(quickGameBtn, customGameBtn, networkGameBtn, settingBtn);
+
+        this.lobbyWrapper.append(this.customGameScreen);
+    }
+
+    private loop = () => {
+        requestAnimationFrame(this.drawClouds.bind(this));
+        setTimeout(this.loop, 40);
+    };
+
+    public start() {}
+
+    createClouds() {
+        this.cloud = new Image();
+        this.cloud.src = '../../assets/lobby/cloud.png';
+        const cloudWidth = 300;
+        const cloudHeight = 150;
+        const quantity = Math.round(this.canvasHeight / cloudHeight);
+        for (let i = 0; i < quantity; i++) {
+            const y = cloudHeight * i;
+            for (let i = 0; i <= 5; i++) {
+                this.clouds.push({
+                    x: Math.round(Math.random() * this.canvasWidth),
+                    y: y,
+                    width: cloudWidth,
+                    height: cloudHeight,
+                    deltaX: Math.round(Math.random()) ? 2 : -2,
+                });
+            }
         }
+    }
 
-        console.log(options);
-
-        this.startGame(options);
+    drawClouds() {
+        if (this.ctx) {
+            this.ctx.fillStyle = '#7AD7FF';
+            this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+            this.clouds.forEach((el: any) => {
+                if (this.ctx) {
+                    el.x += el.deltaX;
+                    if (el.x > this.canvasWidth || el.x < 0) el.deltaX = -el.deltaX;
+                    this.ctx.drawImage(this.cloud, el.x, el.y, el.width, el.height);
+                }
+            });
+        }
     }
 
     private startGame(options: IStartGameOptions) {
