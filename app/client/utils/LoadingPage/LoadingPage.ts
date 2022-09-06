fd9150b31d9960fc0cdf44a5d3b2ccd6e2f8b845
Wsko-12 <vaskovladislav@gmail.com>
2022-09-06 11:53:35 +0300

    feat: add tutorial to LoadingPage

--- app/client/utils/LoadingPage/LoadingPage.ts
+++ app/client/utils/LoadingPage/LoadingPage.ts
@@ -58,7 +58,49 @@ export default class LoadingPage {
         });
         progressContainer.append(bar);
 
+        const tutorial = this.createTutorial();
+        overlay.append(tutorial);
+
         return { element, bar };
+    }
+
+    private static createTutorial() {
+        const element = <HTMLDivElement>PageBuilder.createElement('div', {
+            classes: 'tutor__container',
+        });
+
+        const arrows = this.createTutorialLine('▴▾▸◂', '- Move worm');
+        const jump = this.createTutorialLine('enter', '- Jump (x2 for backflip)');
+        const shoot = this.createTutorialLine('space', '- Shoot (long press for shoot power)');
+        const inventory = this.createTutorialLine('i or click', '- Inventory');
+        const grenade = this.createTutorialLine('g', '- Grenade');
+        const bazooka = this.createTutorialLine('b', '- Bazooka');
+        const mine = this.createTutorialLine('m', '- Mine');
+        const dynamite = this.createTutorialLine('d', '- Dynamite');
+        const holyGrenade = this.createTutorialLine('h', '- Holy grenade');
+
+        element.append(arrows, jump, shoot, inventory, grenade, bazooka, mine, dynamite, holyGrenade);
+        return element;
+    }
+
+    private static createTutorialLine(button: string, text: string) {
+        const element = <HTMLDivElement>PageBuilder.createElement('div', {
+            classes: 'tutor__line',
+        });
+
+        const buttonElement = <HTMLDivElement>PageBuilder.createElement('div', {
+            classes: 'tutor__button',
+            content: button,
+        });
+
+        const textElement = <HTMLDivElement>PageBuilder.createElement('div', {
+            classes: 'tutor__text',
+            content: text,
+        });
+
+        element.append(buttonElement, textElement);
+
+        return element;
     }
 
     // constructor() {
